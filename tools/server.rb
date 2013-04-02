ddb73602239c9009759b4d92936a27673c363965
Toby Cox <toby@carnivallabs.com>
2013-04-02 20:51:15 +0100

    Adding a small webrick server script to host JS that can be injected into the Stripe admin back end

--- tools/server.rb
+++ tools/server.rb
@@ -4,25 +4,15 @@ require 'webrick'
 require 'webrick/https'
 require 'openssl'
 
-
-pkey = cert = cert_name = nil
-begin
-  pkey =
-OpenSSL::PKey::RSA.new(File.open("/etc/apache2/ssl/host.key").read)
-  cert =
-OpenSSL::X509::Certificate.new(File.open("/etc/apache2/ssl/host.key").read)
-rescue
-  $stderr.puts "Switching to use self-signed certificate"
-  cert_name = [ ["C","JP"], ["O","WEBrick.Org"], ["CN", "WWW"] ]
-end
+cert_name = [
+  %w[CN localhost],
+]
 
 s = WEBrick::HTTPServer.new(
   :Port             => 2000, 
   :DocumentRoot     => ".",
   :SSLEnable        => true,
   :SSLVerifyClient  => OpenSSL::SSL::VERIFY_NONE,
-  :SSLCertificate   => cert,
-  :SSLPrivateKey    => pkey,
   :SSLCertName      => cert_name
 )
 trap('INT') { s.stop }
